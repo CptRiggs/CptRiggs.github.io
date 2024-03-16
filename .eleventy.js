9c473dbabd0fe70ae7209456c5f2595ba68231b9
CaptialState <122716901+CaptialState@users.noreply.github.com>
2024-03-16 10:45:30 +0530

    Added luxon for date parsing

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,11 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("css/");
-    eleventyConfig.addPassthroughCopy("images/")
-    eleventyConfig.addPassthroughCopy("graphic/")
+    eleventyConfig.addPassthroughCopy("images/");
+    eleventyConfig.addPassthroughCopy("graphic/");
+
+    eleventyConfig.addFilter("asPostDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toFormat("dd.MM.yy");
+    });
 };
