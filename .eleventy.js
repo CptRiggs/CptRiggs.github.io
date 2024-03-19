2ccb3f53d8bd6aa2e98ebc851bc0b01f7cdd7ddb
CaptialState <122716901+CaptialState@users.noreply.github.com>
2024-03-19 07:35:48 +0530

    Added syntax highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 const { DateTime } = require("luxon");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("css/");
     eleventyConfig.addPassthroughCopy("images/");
     eleventyConfig.addPassthroughCopy("graphic/");
+    eleventyConfig.addPlugin(syntaxHighlight);
 
     eleventyConfig.addFilter("asPostDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toFormat("dd.MM.yy");
